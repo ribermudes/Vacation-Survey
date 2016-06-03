5af1f9c2a673ce1c587643b772752029778b184a
Rachael Bermudes <bermudesorama@gmail.com>
2016-06-03 11:16:05 -0700

    add questions and css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,12 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     event.preventDefault();
-    var name = $("#name").val();
-    var age = $("#age").val();
-    var gender = $("#gender").val();
-    var movie = $("#movie").val();
+    var location = $("input:radio[name=drink]:checked").val();
+    var home = $("input:radio[name=home]:checked").val();
+    var fun = $("input:radio[name=home]:checked").val();
+    var drink = $("input:radio[name=home]:checked").val();
+    var word = $("input:radio[name=word]:checked").val();
 
-    $('#horse').hide();
-
-    if (age < 18) {
-      $('#final').show();
-      $('#underage').show();
-
-    } else if (age > 40 && gender === "male") {
-        $('#final').show();
-        $('#jeff').show();
-        }
-
-      else if (age <= 40 && gender === "male") {
-        $('#final').show();
-        $('#tom').show();
-      }
-
-      else if (age <= 40 && gender === "female") {
-        $('#final').show();
-        $('#emma').show();
-      }
-      else if (age > 40 && gender === "female") {
-        $('#final').show();
-        $('#helen').show();
-      }
-
-      else if (movie === "horror") {
-        $('#final').show();
-        $('#helen').show();
-      }
-
-      else {
-        $('#final').show();
-        $('#horse').show();
-      }
 
 
   });
